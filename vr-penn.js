2f56e283d36d2292c4ae4510738cf8fc903b2a29
Luke Carlson <jlukecarlson@gmail.com>
2016-04-06 16:28:02 -0400

    Added router, restructured page

--- vr-penn.js
+++ vr-penn.js
@@ -1,20 +1,25 @@
 if (Meteor.isClient) {
 
-  Template.scene.helpers({
-      colorPlane: function() {
-          
-          if (Session.get("color-plane")) {
-              return Session.get("color-plane");
-          }
-          return "#7BC8A4";
+    Template.scene.onRendered(function() {
+        console.log(this.data.light);
+    });
 
-      }
-  });
+
+    Template.scene.helpers({
+        colorPlane: function() {
+            
+            if (Session.get("color-plane")) {
+                return Session.get("color-plane");
+            }
+            return "#7BC8A4";
+
+        }
+    });
 
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }
